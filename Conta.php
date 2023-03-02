1ccfe34a5402c8bf9b7fd78baa7b2112cd2982a3
walssimon <walssimon123@gmail.com>
2023-03-01 22:06:58 -0300

    01-03-23

---
+++ Conta.php
@@ -0,0 +1,29 @@
+<?php 
+class Conta
+{
+    var $agencia;
+    var $codigo;
+    var $dataDeCriacao;
+    var $titular;
+    var $senha;
+    var $saldo;
+    var $cancelada;
+
+
+    function retirar($quantia)
+    {
+        if ($quantia > 0){
+            $this->saldo += $quantia;
+        }
+    }
+function depositar($quantia){
+    $this->saldo = $quantia;
+}
+
+function obterSaldo(){
+return $this->saldo;
+    }
+}
+
+
+?>
